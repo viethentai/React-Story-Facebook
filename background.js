5c9ef4d54ff41aad54e12db6b8ab61a68267fa5c
vt.htz_lyp <adadsex1234@gmail.com>
2026-01-17 01:36:07 +0700

    Add files via upload

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url.includes("https://www.facebook.com/stories")) {
+chrome.runtime.onInstalled.addListener()
+chrome.action.setBadgeText({ text: "" });
+if (changeInfo.status === "complete") {
+  if (tab.url && tab.url.startsWith("https://www.facebook.com/stories/")) {
     console.log("📌 Đang inject story.js vào tab:", tab.url);
     chrome.scripting.executeScript({
-      target: { tabId: tabId },
+      target: { tabId },
       files: ["story.js"]
     });
   }
-});
+}
+
